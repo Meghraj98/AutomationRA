9afe7378fd129eae00f5f8982d73227f33a0d797
meghraj.swamy <meghraj.swamy@external.toyota-europe.com>
2024-05-28 23:39:43 +0530

    first commit

---
+++ src/main/java/tests/test_01.java
@@ -0,0 +1,16 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class test_01 {
+
+    public static void main(String[] args) {
+
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getTime());
+        System.out.println("hello");
+
+    }
+}
